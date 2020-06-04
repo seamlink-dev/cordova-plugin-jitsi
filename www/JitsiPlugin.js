f138409f8a1d229c81ce064b4c73010d11fe58c6
seamlink-aalves <49952868+seamlink-aalves@users.noreply.github.com>
2020-06-04 15:24:05 +0100

    Update JitsiPlugin.js

--- www/JitsiPlugin.js
+++ www/JitsiPlugin.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
-exports.join = function(serverUrl, room, audioOnly, success, error) {
-    exec(success, error, "JitsiPlugin", "join", [serverUrl, room, !!audioOnly]);
+exports.join = function(serverUrl, room, audioOnly, token, success, error) {
+    exec(success, error, "JitsiPlugin", "join", [serverUrl, room, !!audioOnly, token]);
 };
 
 exports.destroy = function(success, error) {
